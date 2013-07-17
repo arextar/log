d3f543a9f04ace624cba0fd7b8217ec308f545ec
Alexander Wilson <arexkun11@gmail.com>
2013-07-16 20:05:16 -0400

    add more log methods

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var http = require('http');
+var fs = require('fs');
 require('colors')
 
 var colors = {
-
+	info: 'blue',
+	pass: 'green',
+	warn: 'yellow',
+	err: 'red'
 }
-
-String.prototype.blank
 
 var server = http.createServer(function (req, res) {
 	res.writeHead('200', {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "POST", "Access-Control-Allow-Headers": "Content-Type"})
 	if (req.method === 'OPTIONS') return res.end()
 
 	var type = req.url.slice(1) || 'log'
-	//console.log(type)
 	req.on('data', function (d) {
-		var str = type.bold + '—' + JSON.parse(d).map(function (n) {
+		d = JSON.parse(d + '')
+
+		var str = type.bold + '—' + d.map(function (n) {
 			return JSON.stringify(n)
 		}).join(' ')
 
