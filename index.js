2f4acbe88320c4faa3a6c286a478d6adfa97196b
Alexander Wilson <arexkun11@gmail.com>
2013-07-16 20:09:22 -0400

    use batches to user fewer requests and preserve order of close logs

--- index.js
+++ index.js
@@ -13,21 +13,24 @@ var server = http.createServer(function (req, res) {
 	res.writeHead('200', {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "POST", "Access-Control-Allow-Headers": "Content-Type"})
 	if (req.method === 'OPTIONS') return res.end()
 
-	var type = req.url.slice(1) || 'log'
 	req.on('data', function (d) {
 		d = JSON.parse(d + '')
 
-		var str = type.bold + '—' + d.map(function (n) {
-			return JSON.stringify(n)
-		}).join(' ')
+		d.forEach(function (log) {
+			var str = log.type.bold + '—' + log.data.map(function (n) {
+				return JSON.stringify(n)
+			}).join(' ')
 
-		if (colors[type]) {
-			console.log(str[colors[type]])
-		}
-		else
-		{
-			console.log(str)
-		}
+			if (colors[log.type]) {
+				console.log(str[colors[log.type]])
+			}
+			else
+			{
+				console.log(str)
+			}
+		})
+
+		
 	})
 
 	res.end()
